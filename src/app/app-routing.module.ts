7fa1a4785dde259d8a8ff6fe6f9252e0f7e87a98
jeffssant <santana.jeff@gmail.com>
2021-04-22 18:05:42 -0300

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: PorPaisComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'region',
+        component: PorRegionComponent    
+    },
+    {
+        path: 'capital',
+        component: PorCapitalComponent     
+    },
+    {
+        path: 'pais/:codigoPais',
+        component: PorPaisComponent        
+    },
+
+    {
+        path: '**',
+        redirectTo: ''
+    }
+
+];
+
+
+
+@NgModule({
+    imports:[
+        RouterModule.forRoot(routes)
+    ],
+    exports:[
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
