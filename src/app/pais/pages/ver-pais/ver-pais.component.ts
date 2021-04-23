38d97627be3a80b0ee8f56f2882bed224ae0120e
jeffssant <santana.jeff@gmail.com>
2021-04-23 19:20:37 -0300

    pagina de detalhe do pais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -7,10 +12,19 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country;
 
-  constructor() { }
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private PaisService: PaisService    
+    ) { }
 
   ngOnInit(): void {
+    this.activateRoute.params
+    .pipe(
+      switchMap(({id})=> this.PaisService.getPaisPorAlpha(id)),
+      tap(console.log)
+    )
+    .subscribe(pais => this.pais = pais)
   }
-
 }
