55a38ff674f4a8c2d6cca3aabb80e7a9d6a6f841
jeffssant <santana.jeff@gmail.com>
2021-04-23 16:15:11 -0300

    separaçao dos componentes, interfaces, mostrar paises na tabela

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -10,15 +11,29 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino:string = '';
+  hayError: boolean = false;
+  paises :Country[] = [];
 
   constructor(private paisService: PaisService) { }
 
-  buscar(){
+  buscar(termino: string){
+    this.hayError = false;
+    this.termino = termino;
     console.log(this.termino);
 
-    this.paisService.buscarPais(this.termino).subscribe(resp => {
-      console.log(resp);
-    })
+    this.paisService.buscarPais(this.termino).subscribe(
+
+      (paises) => { 
+        console.log(paises);
+        this.paises = paises;
+      }, 
+
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+
+    )
 
   }
  
